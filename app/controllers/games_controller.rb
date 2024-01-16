413e7daf44a0f7405c490586f1b6bf23c91ae2d6
Guilhem Romo <guilhem.romo@orange.fr>
2024-01-16 01:58:10 +0100

    on avance sur la correction

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,8 +16,26 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    
+    if @errors_array.nil?
+      @translations = database.sample(@game.number_of_words)
+    else
+      # On va chercher dans la database les errors de la précédente game
+      @translations = "Youpi"
+    end
+  end
 
-    @translations = database.sample(@game.number_of_words)
+  def correction
+    # On récupère l'errorsArray
+    @errors_array = JSON.parse(request.body.read)['errors_array']
+    # render json: { message: 'Traitement réussi', data: @errors_array.count }
+    @new_game = Game.new(number_of_words: @errors_array.count)
+
+    if @new_game.save
+      redirect_to game_path(@new_game)
+    else
+      redirect_to root_path
+    end
   end
 
   private
