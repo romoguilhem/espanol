e65691b9d5dca006c65c3ed70f7fed7e603d9d09
Guilhem Romo <guilhem.romo@orange.fr>
2024-02-06 17:24:51 +0100

    problem solved, appli working fine

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -36,17 +36,11 @@ class GamesController < ApplicationController
     # On récupère l'errorsArray
     errors_array = JSON.parse(request.body.read)['errors_array']
 
-    # On check les bugs
-    # render json: { message: 'Traitement réussi', data: errors_array }
-    # Rails.logger.debug("Response: #{response.body}")
-    
     # On lance la game de correction
     @new_game = Game.new(number_of_words: errors_array.count)
 
-    # On stocke les erreurs pour la session
-    # session.now[:array] = errors_array
-
     if @new_game.save
+      # On redirige vers la new game et on envoie l'errors_array
       redirect_to game_path(@new_game, array: errors_array)
     else
       render :new
@@ -65,5 +59,6 @@ class GamesController < ApplicationController
     read = File.read(filepath)
     json = JSON.parse(read)
     return json["translations"]
+    # Avec cette fonction database, on récupère alors un array de hash
   end
 end
